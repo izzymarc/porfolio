0f65da58c9f8907d1552e80b373d8b93a2024643
izzymarc <izzymarc@gmail.com>
2025-03-25 01:11:17 +0100

    Update header name styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -42,7 +42,7 @@ const Header = () => {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="text-2xl font-bold">
-            Ezekiel
+            <span className="text-primary dark:text-white">Ezekiel</span>
           </Link>
 
           {/* Desktop Navigation */}
