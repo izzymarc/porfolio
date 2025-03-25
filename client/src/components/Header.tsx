4481638acd42f15fd11dd22dd046574807866ecf
izzymarc <izzymarc@gmail.com>
2025-03-25 01:02:08 +0100

    Update header to show 'Ezekiel'

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -42,7 +42,7 @@ const Header = () => {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="text-2xl font-bold">
-            {personalInfo.name}
+            Ezekiel
           </Link>
 
           {/* Desktop Navigation */}
